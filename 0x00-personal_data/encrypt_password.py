881acdcc611e4cf203c15912a9e88737b7301655
amineNouabi <aminenouabi9@gmail.com>
2024-11-09 17:27:07 +0100

    personal data 3

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,8 @@ def hash_password(password: str) -> bytes:
     """Hash password function"""
     hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks if password is valid"""
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
