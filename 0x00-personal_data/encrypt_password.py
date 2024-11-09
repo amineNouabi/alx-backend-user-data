a50b21e383355133c97114a89f5fd343610dc8d3
amineNouabi <aminenouabi9@gmail.com>
2024-11-09 16:39:07 +0100

    personal data 3

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,4 +10,4 @@ import bcrypt
 def hash_password(password: str) -> str:
     """Hash password function"""
     hashed = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-    return hashed.decode("utf-8")
+    return hashed
