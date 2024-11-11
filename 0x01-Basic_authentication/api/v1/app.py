c5cd1bc3940376a8d298f33f2678b7e5dfdc6108
amineNouabi <aminenouabi9@gmail.com>
2024-11-11 22:26:01 +0100

    Basic Auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,7 +15,12 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
 
-if os.getenv("AUTH_TYPE") == 'auth':
+auth_type = os.getenv("AUTH_TYPE")
+
+if auth_type == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
