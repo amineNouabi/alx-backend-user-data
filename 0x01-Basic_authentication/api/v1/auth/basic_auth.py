b2893872059349462489595a3c1341d4cf634a5b
amineNouabi <aminenouabi9@gmail.com>
2024-11-12 03:09:35 +0100

    Basic Auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -21,7 +21,7 @@ class BasicAuth(Auth):
         return authorization_header.split(" ", 1)[1]
 
     def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
-        """
+        """ Decodes base64 authorization token
         """
         if base64_authorization_header is None \
             or not isinstance(base64_authorization_header, str):
@@ -30,3 +30,13 @@ class BasicAuth(Auth):
              return b64decode(base64_authorization_header.encode("utf-8")).decode("utf-8")
         except:
              return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """ Extract user credentials
+        """
+        if decoded_base64_authorization_header is None \
+            or not isinstance(decoded_base64_authorization_header, str)\
+            or len(decoded_base64_authorization_header.split(":")) != 2:
+             return (None, None)
+        email, password = decoded_base64_authorization_header.split(":")
+        return email, password
