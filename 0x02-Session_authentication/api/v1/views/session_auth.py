2b28ecf8f6acc1bf185e0490ab3c7d9058ca7741
amineNouabi <aminenouabi9@gmail.com>
2024-11-13 20:47:01 +0100

    Session Auth

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """ Module of Sessiom auth views
 """
+from typing import List
 from flask import jsonify, make_response, request
 from api.v1.views import app_views
 from os import getenv
 from models.user import User
+
 
 @app_views.route('/auth_session/login',
                  methods=['POST'], strict_slashes=False)
@@ -14,20 +16,21 @@ def login():
     email = request.form.get("email")
     password = request.form.get("password")
     if not email:
-        return jsonify({ 'error': 'email missing'}), 400
+        return jsonify({'error': 'email missing'}), 400
     if not password:
-        return jsonify({ 'error': 'password missing'}), 400
+        return jsonify({'error': 'password missing'}), 400
 
     try:
-        user: User = User.search({ email: email })
-        if not user:
-            return jsonify({ 'error': 'no user found for this email'}), 404
-        if not user.is_valid_password(password):
-            return jsonify({ 'error': 'wrong password'}), 401
+        user: List[User] = User.search({'email': email})
+        if not user or not len(user):
+            return jsonify({'error': 'no user found for this email'}), 404
+        if not user[0].is_valid_password(password):
+            return jsonify({'error': 'wrong password'}), 401
 
         from api.v1.app import auth
-        response = make_response(user.to_json())
-        response.set_cookie(getenv("SESSION_NAME"), auth.create_session(user.id))
+        response = make_response(user[0].to_json())
+        response.set_cookie(getenv("SESSION_NAME"),
+                            auth.create_session(user[0].id))
         return response
     except Exception:
-        return jsonify({ 'error': 'no user found for this email'}), 400
+        return jsonify({'error': 'no user found for this email'}), 404
