555b767cb0d5609e6457a295f6a0bcdaa96478b6
amineNouabi <aminenouabi9@gmail.com>
2024-11-13 18:13:26 +0100

    Session Auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -17,12 +17,15 @@ auth = None
 
 auth_type = os.getenv("AUTH_TYPE")
 
-if auth_type == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
-else:
+if auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
+elif auth_type == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif auth_type == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.before_request
---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Module of Sessiom auth views
+"""
+from flask import jsonify, make_response, request
+from api.v1.views import app_views
+from os import getenv
+from models.user import User
+
+@app_views.route('/auth_session/login',
+                 methods=['POST'], strict_slashes=False)
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if not email:
+        return jsonify({ 'error': 'email missing'}), 400
+    if not password:
+        return jsonify({ 'error': 'password missing'}), 400
+
+    user: User = User.search({ email: email })
+    if not user:
+        return jsonify({ 'error': 'no user found for this email'}), 404
+    if not user.is_valid_password(password):
+        return jsonify({ 'error': 'wrong password'}), 401
+
+    from api.v1.app import auth
+    
+    response = make_response(user.to_json())
+    response.set_cookie(getenv("SESSION_NAME"), auth.create_session(user.id))
+    return response
