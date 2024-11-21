a202f7fefb91fe60959e8f89196395173349aacb
amineNouabi <aminenouabi9@gmail.com>
2024-11-21 21:49:27 +0100

    auth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -24,7 +24,7 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email: str, password: str):
+    def register_user(self, email: str, password: str) -> User:
         """ Registers a new user
         """
         try:
