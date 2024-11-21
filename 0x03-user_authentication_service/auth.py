000cfacbbdb6ad1d55fa60bf6d9fd5c6b470fd58
amineNouabi <aminenouabi9@gmail.com>
2024-11-21 21:39:52 +0100

    auth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,8 +2,12 @@
 """ Auth module
 """
 
+
 def _hash_password(password: str) -> str:
     """ Returns a hashed password
     """
     import bcrypt
-    return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+    return bcrypt.hashpw(
+        password.encode(),
+        bcrypt.gensalt()
+    ).decode()
