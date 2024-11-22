16d0e38bad1ce055e12823eca978a0922d7457fa
amineNouabi <aminenouabi9@gmail.com>
2024-11-22 03:24:06 +0100

    auth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -54,3 +54,15 @@ class Auth:
         except NoResultFound:
             pass
         return False
+
+    def create_session(self, email: str) -> str:
+        """ Creates a new session for email.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            pass
+        return None
