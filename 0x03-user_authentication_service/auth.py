4f3dfaf7060468eca6fb88ae22eb1a8dae68c8ef
amineNouabi <aminenouabi9@gmail.com>
2024-11-22 03:15:00 +0100

    auth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -34,3 +34,13 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Except email and password returns validity.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(), user.hashed_password.encode())
+        except NoResultFound:
+            pass
+        return False
